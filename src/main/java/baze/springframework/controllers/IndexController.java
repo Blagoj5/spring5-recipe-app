32cb84fc717b7d10353e9f668d439d5e2777edd2
baze555 <bazeto25@gmail.com>
2019-10-27 14:02:48 +0100

    adding query methods

--- src/main/java/baze/springframework/controllers/IndexController.java
+++ src/main/java/baze/springframework/controllers/IndexController.java
@@ -1,13 +1,34 @@
 package baze.springframework.controllers;
 
+import baze.springframework.domain.Category;
+import baze.springframework.domain.UnitOfMeassure;
+import baze.springframework.repositories.CategoryRepository;
+import baze.springframework.repositories.UnitOfMeassureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeassureRepository unitOfMeassureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeassureRepository unitOfMeassureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeassureRepository = unitOfMeassureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndex(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeassure> unitOfMeassureOptional = unitOfMeassureRepository.findByDescription("Tablespoon");
+
+        System.out.println("Category id: " + categoryOptional.get().getId());
+        System.out.println("UOM id: " + unitOfMeassureOptional.get().getId());
+
         return "index";
     }
 
--- src/main/java/baze/springframework/repositories/UnitOfMeassureRepository.java
+++ src/main/java/baze/springframework/repositories/UnitOfMeassureRepository.java
@@ -3,5 +3,10 @@ package baze.springframework.repositories;
 import baze.springframework.domain.UnitOfMeassure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeassureRepository extends CrudRepository<UnitOfMeassure, Long> {
+
+    Optional<UnitOfMeassure> findByDescription(String description);
+
 }
