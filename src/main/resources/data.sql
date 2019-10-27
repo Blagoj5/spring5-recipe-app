b43af1bce0c0174973c972e86ad1c42c406c3cf6
baze555 <bazeto25@gmail.com>
2019-10-27 13:27:58 +0100

    initializing data with spring

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (description) VALUES ('American');
+INSERT INTO category (description) VALUES ('Italian');
+INSERT INTO category (description) VALUES ('Mexican');
+INSERT INTO category (description) VALUES ('Fast food');
+INSERT INTO unit_of_meassure (description) VALUES ('Teaspoon');
+INSERT INTO unit_of_meassure (description) VALUES ('Tablespoon');
+INSERT INTO unit_of_meassure (description) VALUES ('Cup');
+INSERT INTO unit_of_meassure (description) VALUES ('Pinch');
+INSERT INTO unit_of_meassure (description) VALUES ('Ounce');
