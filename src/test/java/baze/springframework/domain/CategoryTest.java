c1590f141993db3c28d63186a8b38d1b9514105e
baze555 <bazeto25@gmail.com>
2019-11-05 14:22:50 +0100

    implementing simple junit test

---
+++ src/test/java/baze/springframework/domain/CategoryTest.java
@@ -0,0 +1,36 @@
+package baze.springframework.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setUp(){
+        category = new Category();
+    }
+
+
+    @Test
+    public void getId() {
+
+        Long longValue = 4L;
+
+        category.setId(longValue);
+
+        assertEquals(longValue, category.getId());
+
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void getRecipes() {
+    }
+}
